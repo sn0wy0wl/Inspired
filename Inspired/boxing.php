5d67e0b1963c69b9b25dec9ac799daf36ec113d5
sn0wy0wl <45458668+sn0wy0wl@users.noreply.github.com>
2024-09-24 11:01:12 +0100

    Create boxing.php

---
+++ Inspired/boxing.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inspired Gym - Boxing</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <!-- Include Header -->
+    <?php include 'partials/header.php'; ?>
+
+    <section id="boxing">
+        <div class="container">
+            <h2>Boxing</h2>
+            <p>Boxes need doing.</p>
+        </div>
+    </section>
+
+    <!-- Include Footer -->
+    <?php include 'partials/footer.php'; ?>
+
+</body>
+</html>
