4e35266a719aefab654d9460c5d8ece205a16567
sn0wy0wl <45458668+sn0wy0wl@users.noreply.github.com>
2024-09-24 12:07:14 +0100

    Update header.php

--- Inspired/partials/header.php
+++ Inspired/partials/header.php
@@ -13,19 +13,29 @@
             <li><a href="classes.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'classes.php') ? 'active' : ''; ?>">Classes</a></li>
             <li><a href="staff.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'staff.php') ? 'active' : ''; ?> staff-link">Staff</a></li>
             
-            <!-- Dropdown Menu for Training Styles -->
+            <!-- Dropdown menu for Training Styles -->
             <li class="dropdown">
-                <a href="#" class="dropdown-btn">Training Styles</a>
-                <ul class="dropdown-content">
-                    <li><a href="powerlifting.php">Powerlifting</a></li>
-                    <li><a href="strongman.php">Strongman</a></li>
-                    <li><a href="bodybuilding.php">Bodybuilding</a></li>
-                    <li><a href="boxing.php">Boxing</a></li>
-                    <li><a href="classes.php">Classes</a></li>
-                    <li><a href="olympic-lifting.php">Olympic Lifting</a></li>
-                    <li><a href="crossfit.php">Crossfit</a></li>
-                    <li><a href="cardio.php">Cardio Area</a></li>
-                </ul>
+                <a href="#" class="dropbtn">Training Styles</a>
+                <div class="dropdown-content">
+                    <a href="powerlifting.php">Powerlifting</a>
+                    <a href="strongman.php">Strongman</a>
+                    <a href="bodybuilding.php">Bodybuilding</a>
+                    <a href="boxing.php">Boxing</a>
+                    <a href="classes.php">Classes</a>
+                    <a href="olympic-lifting.php">Olympic Lifting</a>
+                    <a href="crossfit.php">Crossfit</a>
+                    <a href="cardio-area.php">Cardio Area</a>
+                </div>
+            </li>
+
+            <!-- Dropdown menu for Stock -->
+            <li class="dropdown">
+                <a href="#" class="dropbtn">Stock</a>
+                <div class="dropdown-content">
+                    <a href="kangen-water.php">Kangen Water</a>
+                    <a href="supplements.php">Supplements</a>
+                    <a href="cafe.php">Cafe (Coming Soon)</a>
+                </div>
             </li>
         </ul>
     </nav>
