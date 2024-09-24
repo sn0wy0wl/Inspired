e0147b153601a8179f6eac40e4b4ca5ae673718d
sn0wy0wl <45458668+sn0wy0wl@users.noreply.github.com>
2024-09-24 12:23:15 +0100

    Update index.php

--- Inspired/index.php
+++ Inspired/index.php
@@ -15,17 +15,12 @@
     <section id="home">
         <div class="container">
             <h2>Welcome to Inspired Gym</h2>
-            <p>Your ultimate fitness destination. From state-of-the-art equipment to expert trainers, we have everything you need to achieve your fitness goals.</p>
-            <h3>Services We Offer:</h3>
-            <ul class="services">
-                <li>Ice therapy</li>
-                <li>Heat therapy</li>
-                <li>thinAir oxygen therapy</li>
-                <li>Personal Trainers for all skill levels</li>
-                <li>Varied Classes</li>
-                <li>In-house masseuse</li>
-                <li>A safe environment to work out for everyone.</li>
-            </ul>
+            
+            <!-- Image replacing the removed text -->
+            <div class="home-image">
+                <img src="images/1000008494.jpg" alt="Inspired Gym Image">
+            </div>
+
             <div class="button-container">
                 <a href="#" class="btn">Join Now</a>
             </div>
